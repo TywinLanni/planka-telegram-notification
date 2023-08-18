8197cc53282e992e345e39a820fe84d196e8237d
Tywin <sasha291096@gmail.com>
2023-08-18 14:02:40 +0300

    Fix mongo lod level Rework planka environment variables

--- src/main/kotlin/tywinlanni/github/com/plankaTelegram/main.kt
+++ src/main/kotlin/tywinlanni/github/com/plankaTelegram/main.kt
@@ -7,25 +7,17 @@ import tywinlanni.github.com.plankaTelegram.planka.PlankaClient
 import tywinlanni.github.com.plankaTelegram.wacher.Watcher
 
 suspend fun main() {
-    val plankaHost = System.getenv("PLANKA_HOST")
-    val plankaPort = System.getenv("PLANKA_PORT")
-    val plankaProtocol = System.getenv("PLANKA_PROTOCOL") ?: "http"
-    val plankaUrl = System.getenv("PLANKA_URL")
-
     val plankaUsername = System.getenv("PLANKA_SERVICE_USERNAME")
     val plankaPassword = System.getenv("PLANKA_SERVICE_PASSWORD")
+    val plankaConnectionString = System.getenv("PLANKA_CONNECTION_STRING")
 
     val mongoDbConnectionString = System.getenv("MONGO_CONNECTION_STRING") ?: "mongodb://localhost:27017"
     val databaseName = System.getenv("DATABASE_NAME") ?: "Planka"
 
     val telegramBotToken = System.getenv("TELEGRAM_BOT_TOKEN")
 
-    require(plankaUrl != null || (plankaHost != null && plankaPort != null)) {
-        "Planka url ot host/port not found"
-    }
-
     val client = PlankaClient(
-        plankaUrl = plankaUrl ?: "$plankaProtocol://$plankaHost:$plankaPort",
+        plankaUrl = plankaConnectionString,
         plankaUsername = plankaUsername,
         plankaPassword = plankaPassword,
     )
@@ -48,7 +40,7 @@ suspend fun main() {
         serviceClient = client,
         dao = dao,
         notificationBot = notificationBot,
-        plankaUrl = plankaUrl ?: "$plankaProtocol://$plankaHost:$plankaPort",
+        plankaUrl = plankaConnectionString,
     ).apply {
         watchJob.start()
         notificationJob.start()
